5b8dad0c5593dd8116664d4d932d1091ffcf5864
jtrares <jasminetrares@gmail.com>
2024-01-11 09:46:19 -0600

    formatted file

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,21 +1,18 @@
-const input = require('readline-sync');
-
+const input = require("readline-sync");
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
-let candidateName = '';
+// TODO 1.1a: Define candidateName //
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? "
-let correctAnswer = "Sally Ride"
-let candidateAnswer = ''
-
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 //TODO: Variables for Part 2
 let questions;
 let correctAnswers;
 let candidateAnswers;
-
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name
@@ -25,22 +22,18 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question)
+  candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
   if (candidateAnswer === correctAnswer) {
-    console.log('Answer is correct') 
-  } 
-  else {
-    console.log('Answer is incorrect')
+    console.log("Answer is correct");
+  } else {
+    console.log("Answer is incorrect");
   }
 
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
+  let grade; //TODO 3.2 use this variable to calculate the candidates score.
 
   return grade;
 }
@@ -48,7 +41,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-  console.log('Hello ' + candidateName);
+  console.log("Hello " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
@@ -63,5 +56,5 @@ module.exports = {
   correctAnswers: correctAnswers,
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
-  runProgram: runProgram
+  runProgram: runProgram,
 };
